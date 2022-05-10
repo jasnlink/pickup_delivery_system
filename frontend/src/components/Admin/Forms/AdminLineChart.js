8c8bab39fad0ebe461acd682b5e7523fe0c07aa5
jasnlink <chem9310@gmail.com>
2022-05-10 18:05:26 -0400

    Update AdminLineChart.js

--- frontend/src/components/Admin/Forms/AdminLineChart.js
+++ frontend/src/components/Admin/Forms/AdminLineChart.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import { 
+	CircularProgress,
+	Fade
+ } from '@mui/material'
+
+import '../styles/Admin.css';
+
 
 function AdminLineChart({data}) {
 
-	const [loading, setLoading] = useState(true)
+	const [loading, setLoading] = useState(false)
 
-	//get max point of an array
+	//chart values
+	const [yDataMax, setYDataMax] = useState(0)
+	const [yChartMax, setYChartMax] = useState(0)
+	const [yDataHalf, setYDataHalf] = useState(0)
+	const [yChartMin, setYChartMin] = useState(0)
+
+	useEffect(() => {
+
+		//set chart sales highest point, data max, data half, chart minimum
+		getMax(data)
+		.then((result) => {
+
+			setYDataMax(result)
+			setYChartMax(result * 1.5)
+			setYDataHalf(result * 0.5)
+			setYChartMin(0)
+
+			setLoading(false)
+
+		})
+
+	},[])
+
+
+	//helper function to get biggest value of an array
 	async function getMax(data) {
 		let result = 0
-		console.log(data)
 		for(let sale of data) {
 			result = Math.max(result, sale.order_total)
 		}
@@ -17,47 +48,62 @@ function AdminLineChart({data}) {
 	}
 
 
-	const SVG_WIDTH = 720;
-	const SVG_HEIGHT = 640;
+	//chart dimensions
+	const chartWidth = 720;
+	const chartHeight = 640;
+	const dataLength = data.length
 
-	// time on X axis
+
+	// X axis -> time
+	// X axis starting point
 	const x0 = 50;
-	const xAxisLength = SVG_WIDTH - x0 * 2;
+	const xAxisLength = chartWidth - x0 * 2;
 
-	// sales on Y axis
+	// Y axis -> sales
+	// Y axis starting point
 	const y0 = 50;
-	const yAxisLength = SVG_HEIGHT - y0 * 2;
+	const yAxisLength = chartHeight - y0 * 2;
 
+	//X and Y axis crossing point
 	const xAxisY = y0 + yAxisLength;
 
-	//set chart sales highest point, data max, data half, chart minimum
-	getMax(data)
-	.then((result) => {
 
-		var yDataMax = result
-		var yChartMax = yDataMax * 1.5
-		var yDataHalf = yDataMax * 0.5
-		var yChartMin = 0
+	//chart generation function
+	function Chart({ children, width, height }) {
+		return (
+			<svg viewBox={`0 0 ${width} ${height}`} width={width} height={height}>
+				{children}
+			</svg>
+		)
+	}
 
-		console.log('yDataMax',yDataMax)
-		console.log('yChartMax',yChartMax)
-		console.log('yDataHalf',yDataHalf)
-		console.log('yChartMin',yChartMin)
+	//point generation function
+	function Point({ x, y, r=4 }) {
+		return (
+			<circle cx={x} cy={y} r={r} />
+		)
+	}
 
-	})
+
 		
 
 	return (
 		<>
-		{loading && (
+		{!!loading && (
 					<Fade in={loading}>
 						<CircularProgress size={64} style={{position: 'relative', top: '50%', left:'50%', marginTop: '-32px', marginLeft: '-32px'}} color="inherit" />
 					</Fade>
-				)}
+		)}
 		{!loading && (
 		<>
 
-		    <svg width={SVG_WIDTH} height={SVG_HEIGHT}>
+			<Chart width={chartWidth} height={chartHeight}>
+				{data.map((element, index) => (
+					<Point x={(chartWidth/dataLength)*index} y={element.order_total} />
+				))}
+			</Chart>
+
+		    <svg width={chartWidth} height={chartHeight}>
 		      {/* X axis */}
 		      <line
 		        x1={x0}
